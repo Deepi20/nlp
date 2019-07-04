59eaafaba6dbfa0546ce0b6d0c97b036e2e5614f
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-03 22:48:55 -0500

    Create tokenizer.py

---
+++ tokenizer.py
@@ -0,0 +1,28 @@
+import json
+
+with open("C:\Users\deepi\Desktop\personal\coursera\nlp\Sarcasm_Headlines_Dataset.json",'r') as f:
+    datastore = json.load(f)
+
+
+sentences = []
+labels = []
+urls = []
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+tokenizer = Tokenizer(oov_taken = "<OOV>")
+tokenizer.fit_on_texts(sentences)
+
+word_index = tokenizer.word_index
+print(len(word_index))
+print(word_index)
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences,padding ='post')
+print(sentences[2])
+print(padded[2])
+print(padded.shape)
