e65b30ce257c111e6dd7d51ef33e6899576323f1
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-06 23:02:23 -0500

    Update bbctext_embedding.py

--- bbctext_embedding.py
+++ bbctext_embedding.py
@@ -103,4 +103,14 @@ num_epochs = 30
 history = model.fit(train_padded, training_label_seq, epochs=num_epochs, validation_data=(validation_padded, validation_label_seq), verbose=2)
 import matplotlib.pyplot as plt
 
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+  
+plot_graphs(history, "acc")
+plot_graphs(history, "loss")
 
