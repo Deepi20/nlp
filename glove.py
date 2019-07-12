72e569b28438b36f34dec37b250f577f7b3c10e7
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-11 23:28:16 -0500

    Update glove.py

--- glove.py
+++ glove.py
@@ -64,3 +64,8 @@ test_sequences = padded[0:split]
 training_sequences = padded[split:training_size]
 test_labels = labels[0:split]
 training_labels = labels[split:training_size]
+print(vocab_size)
+print(word_index['i'])
+# Expected Output
+# 138858
+# 1
