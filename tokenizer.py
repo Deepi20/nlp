77369fe2f8b30eb88b7595200c559eb4cf57a0e1
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-13 23:52:22 -0500

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -16,3 +16,13 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 tokenizer = Tokenizer(oov_taken = "<OOV>")
 tokenizer.fit_on_texts(sentences)
 
+word_index = tokenizer.word_index
+print(len(word_index))
+print(word_index)
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences,padding ='post')
+print(sentences[2])
+print(padded[2])
+print(padded.shape)
+
+
