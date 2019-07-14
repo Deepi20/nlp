806b9882baa7c2668e3946dfdd42f24d9caaeaa0
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-13 23:51:38 -0500

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -13,16 +13,3 @@ for item in datastore:
     urls.append(item['article_link'])
 
 
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-tokenizer = Tokenizer(oov_taken = "<OOV>")
-tokenizer.fit_on_texts(sentences)
-
-word_index = tokenizer.word_index
-print(len(word_index))
-print(word_index)
-sequences = tokenizer.texts_to_sequences(sentences)
-padded = pad_sequences(sequences,padding ='post')
-print(sentences[2])
-print(padded[2])
-print(padded.shape)
