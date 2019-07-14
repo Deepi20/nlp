eb50db5f1845161f00113bf389e35b635fe2d212
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-13 23:51:59 -0500

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -11,5 +11,8 @@ for item in datastore:
     sentences.append(item['headline'])
     labels.append(item['is_sarcastic'])
     urls.append(item['article_link'])
-
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+tokenizer = Tokenizer(oov_taken = "<OOV>")
+tokenizer.fit_on_texts(sentences)
 
