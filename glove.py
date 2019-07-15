95fc02ba829c9e95cebe62f2d260f07fef96d9ec
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-14 23:53:40 -0500

    Update glove.py

--- glove.py
+++ glove.py
@@ -73,10 +73,4 @@ with open('/tmp/glove.6B.100d.txt') as f:
         coefs = np.asarray(values[1:], dtype='float32');
         embeddings_index[word] = coefs;
 embeddings_matrix = np.zeros((vocab_size+1, embedding_dim));
-for word, i in word_index.items():
-    embedding_vector = embeddings_index.get(word);
-    if embedding_vector is not None:
-        embeddings_matrix[i] = embedding_vector;
-
-
 
